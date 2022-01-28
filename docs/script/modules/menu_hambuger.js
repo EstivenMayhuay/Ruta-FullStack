0efa99bfef777590c1ddec00110976dd3f3d0ef2
Estiven Mayhuay <developerwebhairton@gmail.com>
2022-01-28 13:02:48 -0500

    finish the link active in the nav

--- docs/script/modules/menu_hambuger.js
+++ docs/script/modules/menu_hambuger.js
@@ -30,39 +30,57 @@ function activeLinks(links) {
   d.addEventListener("click", (e) => {
     if (e.target.matches(links)) {
       let idLink = e.target.id;
+      let urlLink = e.target.href;
 
-      saveStore(idLink);
+      let dataNav = {
+        id: idLink,
+        url: urlLink,
+      };
 
-      let idLocal = getStore();
+      saveStore(JSON.stringify(dataNav));
+
+      let data = JSON.parse(getStore());
 
       d.querySelectorAll(".link-active").forEach((linkA) => {
         linkA.classList.remove("link-active");
       });
 
-      if (idLink == idLocal) {
+      console.log(data);
+
+      if (idLink == data.id) {
         e.target.classList.add("link-active");
       }
     }
   });
-
-  localStorage.clear();
 }
 
+// save data into localStorage
 function saveStore(data) {
   localStorage.setItem("idStore", data);
 }
 
+// get the data into localStorage
 function getStore() {
   return localStorage.getItem("idStore");
 }
 
+// restore the link active into localStorage
 function restoreLink() {
   const d = document;
+
+  let data = JSON.parse(getStore());
+
   if (localStorage.getItem("idStore") != null) {
-    let linkId = localStorage.getItem("idStore");
-    let linkActive = d.querySelector(`#${linkId}`);
+    let linkActive = d.querySelector(`#${data.id}`);
 
     linkActive.classList.add("link-active");
+
+    let urlCurr = window.location.href;
+
+    if (urlCurr != data.url) {
+      window.location.reload();
+      window.location.replace(data.url);
+    }
   }
 }
 
---
+++ src/markdown/challenges.md
@@ -0,0 +1,23 @@
+# **Challenges**
+
+<ul class="indice">
+  <li><a href="#challenge-js">Challenges JS</a></li>
+  <li><a href="#challenge-css">Challenges CSS3</a></li>
+  <li><a href="#challenge-html">Challenges HTML5</a></li>
+</ul>
+
+<a id="challenge-js"></a>
+
+## **JavaScript**
+
+### **Challenge 01**
+
+> Crear una funcion que reciba como parametro un array de numeros enteros y que obtenga la **suma total** de todos sus elementos. Como un dato adicional, puedes usar el metodo **forEach** y el array puede ser **[10, 20, 40, 50]**
+
+<a id="challenge-css"></a>
+
+## **CSS3**
+
+<a id="challenge-html"></a>
+
+## **HTML5**
