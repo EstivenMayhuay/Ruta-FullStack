5ffd222df500145088e7d268fd93fda8fcc4cd00
Estiven Mayhuay <developerwebhairton@gmail.com>
2022-02-21 17:36:32 -0500

    add the gulp-less and create a task by less

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require("gulp");
 const pug = require("gulp-pug");
+const less = require("gulp-less");
 
 // create task for pug
 gulp.task("pug", function () {
@@ -13,6 +14,17 @@ gulp.task("pug", function () {
     .pipe(gulp.dest("docs/"));
 });
 
-gulp.task("default", function () {
+// create task for less
+gulp.task("less", function () {
+  return gulp.src("src/less/*.less").pipe(less()).pipe(gulp.dest("docs/css"));
+});
+
+// by execute gulp write in the command line the following command "gulp compilePug" o "npm run buildPug"
+gulp.task("compilePug", function () {
   gulp.watch("src/pug/pages/*.pug", gulp.series("pug"));
+});
+
+// by execute gulp write in the command line the following command "gulp compileLess" o "npm run buildLess"
+gulp.task("compileLess", function () {
+  gulp.watch("src/less/*.less", gulp.series("less"));
 });
